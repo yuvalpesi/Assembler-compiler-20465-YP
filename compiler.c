e85ede546e61db35c57b064fbda0eb95d998dfb0
yuvalpesi <yuvalpesi@gmail.com>
2023-07-30 12:26:21 +0300

    add doc

--- compiler.c
+++ compiler.c
@@ -1,9 +1,15 @@
 #include "data.h"
 
+/**
+ * The entry point of the assembler program.
+ *
+ * @param argc Number of command-line arguments.
+ * @param argv Array of command-line argument strings.
+ * @return 0 indicating successful execution of the program.
+ */
 int main(int argc,char **argv){
-    
+
     getFile(argc,argv);
 
     return 0;
 }
-
